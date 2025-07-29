621fd506ef1b67446cadcd2b499c30d4452b7b9f
Yidnekachew Tamiru <130035610+Yidnek-T@users.noreply.github.com>
2025-07-29 17:27:12 +0300

    Update gameUtill.js

--- utill/gameUtill.js
+++ utill/gameUtill.js
@@ -2,71 +2,39 @@ const DrawnNumber = require("../models/drawnNumbers");
 const GameWinner = require("../models/gameWinner");
 const Slip = require("../models/tickets");
 
-const africaCartelas = require('../card/cards');
-const classicCartelas = require('../card/card2');
 
-exports.getCartelaByType = (cartelaType, cartelaIndex) => {
-  if (!['africa', 'classic'].includes(cartelaType)) {
-    throw new Error(`Invalid cartela type: ${cartelaType}. Must be 'africa' or 'classic'.`);
-  }
-  const cartelas = cartelaType === 'classic' ? classicCartelas : africaCartelas;
-  if (!cartelas[cartelaIndex]) {
-    throw new Error(`Invalid cartela index: ${cartelaIndex} for ${cartelaType} type`);
-  }
-  return cartelas[cartelaIndex];
-};
-
-exports.validateCartelaNumbers = (cartela, cartelaType) => {
-  const ranges = {
-    B: [1, 15],
-    I: [16, 30],
-    N: [31, 45],
-    G: [46, 60],
-    O: [61, 75]
-  };
-  for (const column of ['B', 'I', 'N', 'G', 'O']) {
-    if (!cartela[column]) {
-      throw new Error(`Missing column ${column} in ${cartelaType} cartela`);
-    }
-    cartela[column].forEach((num, index) => {
-      if (column === 'N' && index === 2 && num === null) return; // Allow null in N[2]
-      if (typeof num !== 'number' || num < ranges[column][0] || num > ranges[column][1]) {
-        throw new Error(`Invalid number ${num} in column ${column} for ${cartelaType} cartela`);
-      }
-    });
-  }
-  return true;
-};
+const { cardData } = require('../card/cards');
 
 const getDrawnNumberByGameId = async (gameId) => {
   const drawnNumbers = await DrawnNumber.query()
     .where('gameId', gameId)
-    .orderBy('drawTime');
-  return drawnNumbers.map(obj => obj.number);
-};
+    .orderBy('drawTime'); // Optional: Order by drawTime if needed
+  return drawnNumbers.map(obj => obj.number)
+}
 
 const getSelectedNumberByGameId = async (gameId) => {
   const ticket = await Slip.query().where({ gameId }).andWhereNot({ status: 'canceled' });
   return ticket.map((tic) => tic.pickedNumber);
-};
+}
 
 const getWinnerByGameId = async (gameId) => {
   const gameWinner = await GameWinner.query().where({ gameId });
-  const game = await require('../models/game').findById(gameId);
   gameWinner.forEach((gamewin) => {
-    const cartelaType = game?.cartela_type || 'africa'; // Fallback to 'africa' if not found
-    gamewin.card = exports.getCartelaByType(cartelaType, gamewin.number - 1);
-  });
+    gamewin.card = cardData[gamewin?.number - 1];
+  })
   return gameWinner;
-};
+}
 
 const addAdditionalInfoOnGame = async (game) => {
   game.drawnNumbers = await getDrawnNumberByGameId(game.id);
   game.selectedNumbers = await getSelectedNumberByGameId(game.id);
-  game.winner = await getWinnerByGameId(game.id);
-};
+  game.winner = await getWinnerByGameId(game.id)
+}
 
-module.exports = { getCartelaByType, validateCartelaNumbers, addAdditionalInfoOnGame };
+module.exports = { addAdditionalInfoOnGame }
+
+
+
 
 
 
@@ -79,33 +47,76 @@ module.exports = { getCartelaByType, validateCartelaNumbers, addAdditionalInfoOn
 // const GameWinner = require("../models/gameWinner");
 // const Slip = require("../models/tickets");
 
+// const africaCartelas = require('../card/cards');
+// const classicCartelas = require('../card/card2');
 
-// const { cardData } = require('../card/cards');
+// exports.getCartelaByType = (cartelaType, cartelaIndex) => {
+//   if (!['africa', 'classic'].includes(cartelaType)) {
+//     throw new Error(`Invalid cartela type: ${cartelaType}. Must be 'africa' or 'classic'.`);
+//   }
+//   const cartelas = cartelaType === 'classic' ? classicCartelas : africaCartelas;
+//   if (!cartelas[cartelaIndex]) {
+//     throw new Error(`Invalid cartela index: ${cartelaIndex} for ${cartelaType} type`);
+//   }
+//   return cartelas[cartelaIndex];
+// };
+
+// exports.validateCartelaNumbers = (cartela, cartelaType) => {
+//   const ranges = {
+//     B: [1, 15],
+//     I: [16, 30],
+//     N: [31, 45],
+//     G: [46, 60],
+//     O: [61, 75]
+//   };
+//   for (const column of ['B', 'I', 'N', 'G', 'O']) {
+//     if (!cartela[column]) {
+//       throw new Error(`Missing column ${column} in ${cartelaType} cartela`);
+//     }
+//     cartela[column].forEach((num, index) => {
+//       if (column === 'N' && index === 2 && num === null) return; // Allow null in N[2]
+//       if (typeof num !== 'number' || num < ranges[column][0] || num > ranges[column][1]) {
+//         throw new Error(`Invalid number ${num} in column ${column} for ${cartelaType} cartela`);
+//       }
+//     });
+//   }
+//   return true;
+// };
 
 // const getDrawnNumberByGameId = async (gameId) => {
 //   const drawnNumbers = await DrawnNumber.query()
 //     .where('gameId', gameId)
-//     .orderBy('drawTime'); // Optional: Order by drawTime if needed
-//   return drawnNumbers.map(obj => obj.number)
-// }
+//     .orderBy('drawTime');
+//   return drawnNumbers.map(obj => obj.number);
+// };
 
 // const getSelectedNumberByGameId = async (gameId) => {
 //   const ticket = await Slip.query().where({ gameId }).andWhereNot({ status: 'canceled' });
 //   return ticket.map((tic) => tic.pickedNumber);
-// }
+// };
 
 // const getWinnerByGameId = async (gameId) => {
 //   const gameWinner = await GameWinner.query().where({ gameId });
+//   const game = await require('../models/game').findById(gameId);
 //   gameWinner.forEach((gamewin) => {
-//     gamewin.card = cardData[gamewin?.number - 1];
-//   })
+//     const cartelaType = game?.cartela_type || 'africa'; // Fallback to 'africa' if not found
+//     gamewin.card = exports.getCartelaByType(cartelaType, gamewin.number - 1);
+//   });
 //   return gameWinner;
-// }
+// };
 
 // const addAdditionalInfoOnGame = async (game) => {
 //   game.drawnNumbers = await getDrawnNumberByGameId(game.id);
 //   game.selectedNumbers = await getSelectedNumberByGameId(game.id);
-//   game.winner = await getWinnerByGameId(game.id)
-// }
+//   game.winner = await getWinnerByGameId(game.id);
+// };
 
-// module.exports = { addAdditionalInfoOnGame }
+// module.exports = { getCartelaByType, validateCartelaNumbers, addAdditionalInfoOnGame };
+
+
+
+
+
+
+
+
