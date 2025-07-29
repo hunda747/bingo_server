6ad73b3720675c6725cfd843a96fbb8108ba6b07
Yidnekachew Tamiru <130035610+Yidnek-T@users.noreply.github.com>
2025-07-29 17:28:15 +0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,36 +1,44 @@
+
+routes/shopOwners.js
 const express = require('express');
-const adminController = require('../controllers/admin_controller');
+const adminController = require('../controllers/AdminController');
 const { authenticateToken } = require('../middleware/authHandler');
 
 const router = express.Router();
 
+router.get('/', adminController.getAll);
+router.get('/:id', adminController.getById);
+router.post('/', adminController.create);
 router.post('/login', adminController.login);
-router.post('/refresh', authenticateToken, adminController.refreshToken);
-router.get('/', authenticateToken, adminController.getAll);
-router.get('/:id', authenticateToken, adminController.getById);
-router.post('/', authenticateToken, adminController.create);
-router.post('/changePassword/:id', authenticateToken, adminController.changePassword);
-router.put('/:id', authenticateToken, adminController.update);
-router.delete('/:id', authenticateToken, adminController.delete);
+router.post('/changePassword/:id', adminController.changePassword);
+router.post('/refresh', adminController.refreshToken);
+router.put('/:id', adminController.update);
+router.delete('/:id', adminController.delete);
 
 module.exports = router;
 
 
 
-// routes/shopOwners.js
+
+
+
+
+
 // const express = require('express');
-// const adminController = require('../controllers/AdminController');
+// const adminController = require('../controllers/admin_controller');
 // const { authenticateToken } = require('../middleware/authHandler');
 
 // const router = express.Router();
 
-// router.get('/', adminController.getAll);
-// router.get('/:id', adminController.getById);
-// router.post('/', adminController.create);
 // router.post('/login', adminController.login);
-// router.post('/changePassword/:id', adminController.changePassword);
-// router.post('/refresh', adminController.refreshToken);
-// router.put('/:id', adminController.update);
-// router.delete('/:id', adminController.delete);
+// router.post('/refresh', authenticateToken, adminController.refreshToken);
+// router.get('/', authenticateToken, adminController.getAll);
+// router.get('/:id', authenticateToken, adminController.getById);
+// router.post('/', authenticateToken, adminController.create);
+// router.post('/changePassword/:id', authenticateToken, adminController.changePassword);
+// router.put('/:id', authenticateToken, adminController.update);
+// router.delete('/:id', authenticateToken, adminController.delete);
 
 // module.exports = router;
+
+
